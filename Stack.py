26f48cf9d889dcfe703f0fc4ae30cedcf6e50acf
cgc96 <59466889+cgc96@users.noreply.github.com>
2020-01-08 17:08:43 +0900

    Add files via upload

---
+++ Stack.py
@@ -0,0 +1,83 @@
+
+#파이썬으로 만든 stack class
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        if not self.head:
+            return True
+        return False
+
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def pop(self):
+        if self.is_empty():
+            return None
+
+        ret_data = self.head.data
+        self.head = self.head.next
+        return ret_data
+
+    def top(self):
+        if self.is_empty():
+            return None
+
+        return self.head.data
+
+#위의 class를 이용 백준 1874번 문제(스택 수열) 풀이
+
+if __name__ == "__main__":
+    s = Stack()
+
+    n = int(input())
+    numbers = []
+    answer = []
+
+    cur = 0
+
+    for a in range(0, n):
+        x = int(input())
+        numbers.append(x)
+
+    i = 1
+
+    while not i == n+1:
+        if s.is_empty() :
+            answer.append('+')
+            s.push(i)
+            i += 1
+
+        elif s.top() == numbers[cur]:
+            answer.append('-')
+            s.pop()
+            cur += 1
+        else :
+            answer.append('+')
+            s.push(i)
+            i += 1
+
+    checker = True
+    while not s.is_empty() :
+        if s.top() == numbers[cur]:
+            cur += 1
+            answer.append('-')
+            s.pop()
+        else:
+            checker = False
+            break
+
+    if checker == True:
+        for i in range(0, len(answer)):
+             print( answer[i])
+    else :
+        print('NO')
